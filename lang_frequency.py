7beb117a0cdf3b54457afb39f8196edee9826519
Sir Nightmare <ivantochilkin@gmail.com>
2016-09-24 05:28:47 +0300

    the task is done

--- lang_frequency.py
+++ lang_frequency.py
@@ -1,12 +1,23 @@
+import re
+from collections import Counter
 
 
 def load_data(filepath):
-    pass
+    data = ""
+    with open(filepath, 'r', encoding="utf8") as file:
+        data = file.read()
+    return data
 
 
 def get_most_frequent_words(text):
-    pass
+    words = re.findall(r'\w+', data.lower())
+    ten_words = Counter(words).most_common(10)
+    print('Ten the most popular words in the text')
+    for key, value in ten_words:
+        print(key, value)
 
 
 if __name__ == '__main__':
-    pass
+    # data = load_data('text.txt')
+    data = load_data(input('Type the path to text file'))
+    get_most_frequent_words(data)
