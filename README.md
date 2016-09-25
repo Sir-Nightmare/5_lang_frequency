d8eccb837c2d6dc65614c8a8746f19c4587f1077
Sir Nightmare <ivantochilkin@gmail.com>
2016-09-25 17:23:28 +0300

    the task was fixed

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # 5_lang_frequency
+This script finds 10 the most 
+
+You can run it by the command:
+ 
+python lang_frequency.py \<path_to_text\>
--- lang_frequency.py
+++ lang_frequency.py
@@ -1,9 +1,9 @@
 import re
 from collections import Counter
+import sys
 
 
 def load_data(filepath):
-    data = ""
     with open(filepath, 'r', encoding="utf8") as file:
         data = file.read()
     return data
@@ -11,13 +11,17 @@ def load_data(filepath):
 
 def get_most_frequent_words(text):
     words = re.findall(r'\w+', text.lower())
-    ten_words = Counter(words).most_common(10)
-    print('Ten the most popular words in the text')
-    for key, value in ten_words:
+    number_of_most_frequent_words = 10
+    return Counter(words).most_common(number_of_most_frequent_words)
+
+
+def print_the_words(words_frequency):
+    print('The most frequent words in the text are:')
+    for key, value in words_frequency:
         print(key, value)
 
-
 if __name__ == '__main__':
-    # data = load_data('text.txt')
-    data = load_data(input('Type the path to text file'))
-    get_most_frequent_words(data)
+    filepath = sys.argv[1]
+    text = load_data(filepath)
+    words_frequency =get_most_frequent_words(text)
+    print_the_words(words_frequency)
